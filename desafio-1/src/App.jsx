6447c0a6254845cf748e41c25b79bf4723d30b3c
Gabriel Matos <gsilva505@hotmail.com>
2022-10-07 23:08:45 -0300

    Alterando animalServices para object para passar no test

--- desafio-1/src/App.jsx
+++ desafio-1/src/App.jsx
@@ -13,7 +13,8 @@ export default function App() {
 	};
 
 	useEffect(() => {
-		AnimalServices()
+		AnimalServices
+			.getAnimals()
 			.then(setList);
 	}, []);
 
--- desafio-1/src/utils/AnimalServices.js
+++ desafio-1/src/utils/AnimalServices.js
@@ -1,10 +1,14 @@
-export default async function AnimalServices() {
-	const API_URL = 'https://zoo-animal-api.herokuapp.com/animals/rand/10';
-	try {
-		return await fetch(API_URL)
-			.then(res => res.json());
-	}
-	catch (error) {
-		console.log('Error:', error);
+let AnimalServices = {
+	async getAnimals() {
+		try {
+			const API_URL = 'https://zoo-animal-api.herokuapp.com/animals/rand/10';
+			return await fetch(API_URL)
+				.then(res => res.json());
+		}
+		catch (e) {
+			console.log('Error:', e);
+		}
 	}
 }
+
+export default AnimalServices
