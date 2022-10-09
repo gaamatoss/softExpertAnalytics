460282b03d37f0984f74f840f79ca4a439faea90
Gabriel Matos <gsilva505@hotmail.com>
2022-10-09 15:10:12 -0300

    implementando paginacao na tabela de paises

--- desafio-2/desafio-2/src/components/countryTable/CountryTable.jsx
+++ desafio-2/desafio-2/src/components/countryTable/CountryTable.jsx
@@ -2,18 +2,33 @@ import React, { useEffect, useState } from 'react';
 import ApiService from '../../controller/ApiService'
 import './countrytable.css'
 
-export default function CountryTable() {
+export default function CountryTable(props) {
 
     const [list, setList] = useState([])
+    const [itensPerPage, setItensPerPage] = useState(15)
+    const [currentPage, setCurrentPage] = useState(0)
+
+    const pages = Math.ceil(list.length / itensPerPage)
+    const startIndex = currentPage * itensPerPage
+    const endIndex = startIndex + itensPerPage
+    const currentItens = list.slice(startIndex, endIndex)
 
     useEffect(() => {
         ApiService()
             .then(setList)
     }, [])
-    console.log('Lista:', list);
 
     return (
         <div className="countrytable">
+            <div>
+                {Array.from(Array(pages), (item, index) => {
+                    return <button
+                        className='pagination__btn'
+                        value={index}
+                        onClick={(e) => setCurrentPage(Number(e.target.value))}
+                    >{index + 1}</button>
+                })}
+            </div>
             <table className="table">
                 <thead className="table__head">
                     <tr style={{ display: 'flex', justifyContent: 'space-between', padding: '10px' }}>
@@ -23,7 +38,7 @@ export default function CountryTable() {
                     </tr>
                 </thead>
                 <tbody className="table__body">
-                    {list.map(item => {
+                    {currentItens.map(item => {
                         return (
                             <tr style={{ display: 'flex', justifyContent: 'space-between', padding: '10px' }}>
                                 <td><input type="checkbox" /></td>
--- desafio-2/desafio-2/src/views/TelaPrincipal.jsx
+++ desafio-2/desafio-2/src/views/TelaPrincipal.jsx
@@ -6,7 +6,7 @@ import { Input, Button } from 'rsuite';
 export default function TelaPrincipal() {
     return (
         <div style={{ width: '100%', display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: "column" }}>
-            <div style={{ width: '20%', display: 'flex', paddingTop: '20px' }}>
+            <div style={{ width: '20%', display: 'flex', padding: '20px 0px' }}>
                 <Input placeholder="Search Country" style={{ width: '100%', marginRight: '10px' }} />
                 <Button appearance="primary" style={{ width: '20%' }}>View</Button>
             </div>
