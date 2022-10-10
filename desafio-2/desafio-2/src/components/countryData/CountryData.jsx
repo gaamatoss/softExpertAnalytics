506b8d2c05bd55fe1cd6f71112e4d7fc606da66b
Gabriel Matos <gsilva505@hotmail.com>
2022-10-10 13:52:11 -0300

    adicionando charts com dados ficticios

--- desafio-2/desafio-2/src/components/countryData/CountryData.jsx
+++ desafio-2/desafio-2/src/components/countryData/CountryData.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import Highcharts from 'highcharts'
 import HighchartsReact from 'highcharts-react-official'
+import { Panel, Button } from 'rsuite';
+import { Link } from 'react-router-dom'
 import './countryData.css'
 
 const chartOptions = {
+    title: {
+        text: ''
+    },
     chart: {
         type: 'column'
     },
     series: [
         {
-            name: 'Profit',
+            name: 'N° de Casos',
             data: [100, 200, 30, 100, 50, 60]
         }
     ]
@@ -18,8 +23,19 @@ const chartOptions = {
 export default function CountryData() {
 
     return (
-        <div style={{ width: '80%' }}>
-            <HighchartsReact highcharts={Highcharts} options={chartOptions} />
+        <div className='mainData'>
+            <Panel header="Brasil" bordered className='panelData' >
+                <HighchartsReact highcharts={Highcharts} options={chartOptions} />
+                <Link to={'/detalhamento'}><Button appearance='primary'>View Detail</Button></Link>
+            </Panel>
+            <Panel header="Argentina" bordered className='panelData' >
+                <HighchartsReact highcharts={Highcharts} options={chartOptions} />
+                <Link to={'/detalhamento'}><Button appearance='primary'>View Detail</Button></Link>
+            </Panel>
+            <Panel header="Chile" bordered className='panelData'>
+                <HighchartsReact highcharts={Highcharts} options={chartOptions} />
+                <Link to={'/detalhamento'}><Button appearance='primary'>View Detail</Button></Link>
+            </Panel>
         </div>
     )
 }
